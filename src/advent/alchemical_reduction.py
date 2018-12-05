7fc0225da2598fe49e2355c3867c37c8ea5eec95
nullus <dylan.perry@gmail.com>
2018-12-05 20:38:12 +1100

    Day 5
    Simplify and cleanup

--- src/advent/alchemical_reduction.py
+++ src/advent/alchemical_reduction.py
@@ -3,29 +3,33 @@
 # Copyright (c) 2018, Dylan Perry <dylan.perry@gmail.com>. All rights reserved.
 # Licensed under BSD 2-Clause License. See LICENSE file for full license.
 
-from typing import Optional, Callable, TypeVar
-
-T = TypeVar('T')
+from typing import List
 
 
-def part1(polymer_input: str, filter_function: Optional[Callable[[T], T]] = None) -> int:
-    polymer = list(filter(filter_function, polymer_input))
+def _react_list(polymer_list: List[str]) -> List[str]:
+    # Don't mutate argument
+    result = polymer_list.copy()
 
-    i, length = 0, len(polymer)
-    while i < length - 1:
-        if polymer[i].swapcase() == polymer[i+1]:
-            del polymer[i:i+2]
-            length -= 2
+    i = 0
+    while i < len(result) - 1:
+        if result[i].swapcase() == result[i + 1]:
+            del result[i:i + 2]
             if i > 0:
                 i -= 1
         else:
             i += 1
 
-    return length
+    return result
+
+
+def part1(polymer_input: str) -> int:
+    return len(_react_list(list(polymer_input)))
 
 
 def part2(polymer_input: str) -> int:
+    polymer_base = _react_list(list(polymer_input))
+
     return min(
-        part1(polymer_input, lambda x: x.lower() != i)
+        len(_react_list([j for j in polymer_base if j.lower() != i]))
         for i in set(polymer_input.lower())
     )
