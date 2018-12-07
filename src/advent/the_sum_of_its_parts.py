325853890f8b00a8735292fd6a0fa0ab574f8f04
nullus <dylan.perry@gmail.com>
2018-12-07 22:55:51 +1100

    Day 7
    Move to common function

--- src/advent/the_sum_of_its_parts.py
+++ src/advent/the_sum_of_its_parts.py
@@ -7,52 +7,47 @@ from typing import Dict, Set, List, Tuple
 from collections import defaultdict
 
 
-def part1(instructions_text: str) -> str:
-    depends: Dict[str, List[str]] = defaultdict(list)
+def _parse(instructions_text: str) -> Tuple[Set[str], Dict[str, List[str]]]:
+    dependencies: Dict[str, List[str]] = defaultdict(list)
     steps: Set[str] = set()
 
     for line in instructions_text.strip().splitlines():
-        steps.add(line[5])
-        steps.add(line[36])
-        depends[line[36]].append(line[5])
+        dependency, step = line[5], line[36]
+        steps.add(step)
+        steps.add(dependency)
+        dependencies[step].append(dependency)
 
-    order = ""
-    while steps:
-        next_step = min(step for step in steps if step not in depends or not depends[step])
-        steps.remove(next_step)
-        depends = dict((k, [i for i in v if i != next_step]) for k, v in depends.items())
-        order += next_step
+    return steps, dependencies
 
-    return order
+
+def _common(instructions_text: str, parallelism: int, ticks_offset: int) -> Tuple[int, str]:
+    steps_remaining, dependencies = _parse(instructions_text)
+
+    worker_assignment: List[Tuple[str, int]] = []
+    ticks, order = 0, ""
+    while steps_remaining or worker_assignment:
+        # Assign jobs
+        while len(worker_assignment) < parallelism:
+            try:
+                step = min(step for step in steps_remaining if step not in dependencies or not dependencies[step])
+            except ValueError:
+                break
+            steps_remaining.remove(step)
+            worker_assignment.append((step, ticks + ticks_offset + ord(step) - 64))
+
+        # Find next finishing tasks
+        ticks = min(job[1] for job in worker_assignment)
+        for finishing_task in sorted([job for job in worker_assignment if job[1] <= ticks], key=lambda k: k[0]):
+            order += finishing_task[0]
+            worker_assignment.remove(finishing_task)
+            dependencies = dict((k, [i for i in v if i != finishing_task[0]]) for k, v in dependencies.items())
+
+    return ticks, order
+
+
+def part1(instructions_text: str) -> str:
+    return _common(instructions_text, 1, 0)[1]
 
 
 def part2(instructions_text: str, workers: int = 5, base_time: int = 60) -> int:
-    depends: Dict[str, List[str]] = defaultdict(list)
-    steps: Set[str] = set()
-
-    for line in instructions_text.strip().splitlines():
-        steps.add(line[5])
-        steps.add(line[36])
-        depends[line[36]].append(line[5])
-
-    worker_assignment: List[Tuple[str, int]] = []
-    order = ""
-    current_time = 0
-    while steps or worker_assignment:
-        # Assign jobs
-        while len(worker_assignment) < workers:
-            try:
-                next_step = min(step for step in steps if step not in depends or not depends[step])
-            except ValueError:
-                break
-            steps.remove(next_step)
-            worker_assignment.append((next_step, current_time + base_time + ord(next_step) - 64))
-
-        # Find next finishing tasks
-        current_time = min(job[1] for job in worker_assignment)
-        for finishing_task in sorted([job for job in worker_assignment if job[1] <= current_time], key=lambda k: k[0]):
-            order += finishing_task[0]
-            worker_assignment.remove(finishing_task)
-            depends = dict((k, [i for i in v if i != finishing_task[0]]) for k, v in depends.items())
-
-    return current_time
+    return _common(instructions_text, workers, base_time)[0]
