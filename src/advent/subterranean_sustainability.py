5d1a031cdca4118ca25821161190af4f0f7a2181
nullus <dylan.perry@gmail.com>
2018-12-15 14:42:26 +1100

    Generalise 1-D automata to identify stable states

--- src/advent/subterranean_sustainability.py
+++ src/advent/subterranean_sustainability.py
@@ -4,7 +4,7 @@
 # Licensed under BSD 2-Clause License. See LICENSE file for full license.
 
 from collections import defaultdict, deque
-from itertools import chain
+from itertools import chain, count
 from typing import List, Dict, Tuple, Iterator
 
 
@@ -19,7 +19,7 @@ def _parser(growth_rules: str) -> Tuple[List[bool], Dict[Tuple[bool, ...], bool]
     return initial_state, rules
 
 
-def _next_state(state: List[bool], rules: Dict[Tuple[bool, ...], bool], offset: int = 0) -> List[bool]:
+def _next_state(state: List[bool], rules: Dict[Tuple[bool, ...], bool], offset: int = 0) -> Tuple[List[bool], int]:
     # Use fixed size deque and chain to window over state
     def window_state(window_size: int = 5):
         padding: List[bool] = [False] * (window_size - 1)
@@ -30,31 +30,30 @@ def _next_state(state: List[bool], rules: Dict[Tuple[bool, ...], bool], offset: 
             yield tuple(window)
 
     next_state: List[bool] = [rules[i] for i in window_state(5)]
+    first_true: int = next_state.index(True)
+    last_true: int = next(1 - i for i in count(1) if next_state[-i])
 
-    return next_state
+    return next_state[first_true:last_true], offset - 2 + first_true
 
 
 def _print_state(state: List[bool]):
     print(''.join({True: '#', False: '.'}[i] for i in state))
 
 
-def part1(growth_rules: str) -> int:
+def _final_state(state: List[bool], rules: Dict[Tuple[bool, ...], bool], generations: int) -> Tuple[List[bool], int]:
+    offset: int = 0
+    for generation in range(generations):
+        next_state, next_offset = _next_state(state, rules, offset)
+        if next_state == state:
+            # Calculate final offset and return if we reach a stable state
+            return state, offset + (next_offset - offset) * (generations - generation)
+        state, offset = next_state, next_offset
+
+    return state, offset
+
+
+def answer(growth_rules: str, generations: int) -> int:
     state, rules = _parser(growth_rules)
-    left_index: int = 0
-    for generation in range(20):
-        state = _next_state(state, rules)
-        left_index += -2 + state.index(True)
-        state = state[state.index(True):]
+    final_state, offset = _final_state(state, rules, generations)
 
-    return sum((i + left_index) for i in range(0, len(state)) if state[i])
-
-
-def part2(growth_rules: str) -> int:
-    state, rules = _parser(growth_rules)
-    left_index: int = 0
-    for generation in range(200):
-        state = _next_state(state, rules)
-        left_index += -2 + state.index(True)
-        state = state[state.index(True):]
-
-    return (50_000_000_000 - 200) * 38 + sum((i + left_index) for i in range(0, len(state)) if state[i])
+    return sum((i + offset) for i in range(0, len(final_state)) if final_state[i])
--- tests/test_subterranean_sustainability.py
+++ tests/test_subterranean_sustainability.py
@@ -4,7 +4,7 @@
 # Licensed under BSD 2-Clause License. See LICENSE file for full license.
 
 from advent.input import text
-from advent.subterranean_sustainability import part1, part2
+from advent.subterranean_sustainability import answer
 
 test_data = """
 initial state: #..#.#..##......###...###
@@ -27,12 +27,12 @@ initial state: #..#.#..##......###...###
 
 
 def test_part1():
-    assert 325 == part1(test_data)
+    assert 325 == answer(test_data, 20)
 
 
 def test_part1_with_puzzle_data():
-    assert 2140 == part1(text('subterranean_sustainability'))
+    assert 2140 == answer(text('subterranean_sustainability'), 20)
 
 
 def test_part2_with_puzzle_data():
-    assert 1900000000384 == part2(text('subterranean_sustainability'))
+    assert 1900000000384 == answer(text('subterranean_sustainability'), 50_000_000_000)
