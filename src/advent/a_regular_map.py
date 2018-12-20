aad7319c7e486a9e3e2ddbae6c90767a0c8d572d
nullus <dylan.perry@gmail.com>
2018-12-21 09:49:39 +1100

    Day 20
    Remove everything we didn't really need to begin with :|
    
    Recursive parsing/mutable structure makes it simpler than I initially
    thought. The way the branches is structured doesn't require following
    multiple paths

--- src/advent/a_regular_map.py
+++ src/advent/a_regular_map.py
@@ -3,21 +3,9 @@
 # Copyright (c) 2018, Dylan Perry <dylan.perry@gmail.com>. All rights reserved.
 # Licensed under BSD 2-Clause License. See LICENSE file for full license.
 
-from collections import defaultdict
+from math import inf
 from operator import add
-from typing import List, Tuple, Dict, Iterator, Set
-
-
-class Node(object):
-    def __init__(self) -> None:
-        super().__init__()
-        self.north = None
-        self.south = None
-        self.east = None
-        self.west = None
-        # Large enough for now
-        self.distance = 0
-
+from typing import Tuple, Dict, Iterator
 
 direction_to_vector = {
     'N': (0, -1),
@@ -27,73 +15,51 @@ direction_to_vector = {
  }
 
 
-def process_map_path(map_regex_iter: Iterator[str], map_nodes: Dict[Tuple[int, int], Node],
-                     start_locations: Set[Tuple[int, int]], start_distance: int = 0):
+def process_map_path(map_regex_iter: Iterator[str], map_nodes: Dict[Tuple[int, int], int],
+                     start_location: Tuple[int, int], start_distance: int = 0) -> None:
 
-    locations = start_locations
+    location = start_location
     distance = start_distance
 
     while True:
         try:
-            next_token = next(map_regex_iter)
+            token = next(map_regex_iter)
         except StopIteration:
-            return set()
-
-        if next_token in ('N', 'E', 'S', 'W'):
+            break
+        if token in ('N', 'E', 'S', 'W'):
             # Next iteration of location data
-            next_locations: List[Tuple[int, int]] = [
-                tuple(map(add, i, direction_to_vector[next_token]))
-                for i in locations
-            ]
-            for location, next_location in zip(locations, next_locations):
-                if map_nodes[next_location].distance:
-                    map_nodes[next_location].distance = min(distance + 1, map_nodes[next_location].distance)
-                else:
-                    map_nodes[next_location].distance = distance + 1
-                if next_token == 'N':
-                    map_nodes[location].north = map_nodes[next_location]
-                    map_nodes[next_location].south = map_nodes[location]
-                elif next_token == 'E':
-                    map_nodes[location].east = map_nodes[next_location]
-                    map_nodes[next_location].west = map_nodes[location]
-                elif next_token == 'S':
-                    map_nodes[location].south = map_nodes[next_location]
-                    map_nodes[next_location].north = map_nodes[location]
-                elif next_token == 'W':
-                    map_nodes[location].west = map_nodes[next_location]
-                    map_nodes[next_location].east = map_nodes[location]
+            next_location: Tuple[int, int] = tuple(map(add, location, direction_to_vector[token]))
+            map_nodes[next_location] = min(distance + 1, map_nodes.get(next_location, inf))
             distance += 1
-            locations = set(next_locations)
-        elif next_token == '(':
-            locations = locations.union(process_map_path(map_regex_iter, map_nodes, locations, distance))
-        elif next_token == '|':
-            return locations.union(process_map_path(map_regex_iter, map_nodes, start_locations, start_distance))
-        elif next_token == ')' or next_token == '$':
-            return locations
+            location = next_location
+        elif token == '(':
+            process_map_path(map_regex_iter, map_nodes, location, distance)
+        elif token == '|':
+            process_map_path(map_regex_iter, map_nodes, start_location, start_distance)
+            break
+        elif token == ')' or token == '$':
+            break
         else:
-            raise RuntimeError(f'Unexpected token: {next_token}')
+            raise RuntimeError(f'Unexpected token: {token}')
 
 
-def process_map_regex(map_regex_iter: Iterator[str]) -> Dict[Tuple[int, int], Node]:
+def process_map_regex(map_regex_iter: Iterator[str]) -> Dict[Tuple[int, int], int]:
     while True:
-        next_token = next(map_regex_iter)
-        if next_token == '^':
-            map_nodes = defaultdict(lambda: Node())
-            process_map_path(map_regex_iter, map_nodes, {(0, 0)})
+        token = next(map_regex_iter)
+        if token == '^':
+            map_nodes = {}
+            process_map_path(map_regex_iter, map_nodes, (0, 0))
             return map_nodes
         else:
-            raise RuntimeError('Unexpected token')
+            raise RuntimeError(f'Unexpected token: {token}')
 
 
 def part1(map_regex: str) -> int:
     map_nodes = process_map_regex(iter(map_regex))
-
-    max_node = max(map_nodes.values(), key=lambda x: x.distance)
-
-    return max_node.distance
+    max_node = max(map_nodes.values())
+    return max_node
 
 
 def part2(map_regex: str) -> int:
     map_nodes = process_map_regex(iter(map_regex))
-
-    return len([i for i in map_nodes.values() if i.distance >= 1000])
+    return len([i for i in map_nodes.values() if i >= 1000])
--- tests/test_a_regular_map.py
+++ tests/test_a_regular_map.py
@@ -5,7 +5,8 @@
 
 from pytest import mark
 
-from advent.a_regular_map import part1
+from advent.a_regular_map import part1, part2
+from advent.input import text
 
 test_data = [
     ["^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$", 23],
@@ -16,3 +17,11 @@ test_data = [
 @mark.parametrize("map_regex, furthest_room", test_data)
 def test_part1(map_regex, furthest_room):
     assert furthest_room == part1(map_regex)
+
+
+def test_part1_with_puzzle_input():
+    assert 3633 == part1(text("a_regular_map"))
+
+
+def test_part2_with_puzzle_input():
+    assert 8756 == part2(text("a_regular_map"))
